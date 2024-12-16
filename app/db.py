cfa1d8f1598910222e5f65e91afba40502498891
pecheritsa <pecheritsa.roman@gmail.com>
2024-12-16 21:09:49 +0300

    add MongoDb connection settings

---
+++ app/db.py
@@ -0,0 +1,15 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+
+from app.config import MONGODB_NAME, MONGODB_URI
+
+
+class MongoDBConnection:
+    def __init__(self, uri: str, database_name: str):
+        self.client = AsyncIOMotorClient(uri)
+        self.db = self.client[database_name]
+
+    async def close(self):
+        self.client.close()
+
+
+db_connection = MongoDBConnection(MONGODB_URI, MONGODB_NAME)
