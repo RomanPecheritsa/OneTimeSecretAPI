5461605f33f468a97e0e5bf2ae174095cb1950b8
pecheritsa <pecheritsa.roman@gmail.com>
2024-12-17 01:11:51 +0300

    include user_router, rename route

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
-from app.routes.secrets_route import router
+
+from app.routes.secret_route import secret_router
+from app.routes.user_route import user_router
 
 app = FastAPI(title="One Time Secret")
 
-app.include_router(router, prefix="")
+app.include_router(secret_router, prefix="")
+app.include_router(user_router, prefix="/auth")
