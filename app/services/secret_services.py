cad5da50e8b46722584f90e10d344c20b5cd81a1
pecheritsa <pecheritsa.roman@gmail.com>
2024-12-17 00:41:39 +0300

    changed ttl indexes, rename using method

--- app/services/secret_services.py
+++ app/services/secret_services.py
@@ -1,18 +1,19 @@
 import uuid
-from datetime import datetime, timezone
+from datetime import datetime, timedelta, timezone
 from typing import Optional
 
 from cryptography.fernet import InvalidToken
 from fastapi import HTTPException, status
 
-from app.models.secrets import Secret
+from app.config import TTL_EXPIRE_SECONDS
+from app.models.secret_model import Secret
 from app.utils.encryption import decrypt, encrypt, generate_key_from_passphrase
 
 
 class SecretService:
     def __init__(self, salt: str, repository) -> None:
         self.salt = salt.encode()
-        self.repository = repository
+        self.__repository = repository
 
     async def generate_key(self, passphrase: str) -> bytes:
         return generate_key_from_passphrase(passphrase.encode(), self.salt)
@@ -21,12 +22,16 @@ class SecretService:
         key = await self.generate_key(passphrase)
         encrypted_secret = encrypt(secret, key)
         secret_key = str(uuid.uuid4())
-        secret_instance = Secret(secret_key=secret_key, secret=encrypted_secret, expiration=datetime.now(timezone.utc))
-        await self.repository.create(secret_instance)
+        secret_instance = Secret(
+            secret_key=secret_key,
+            secret=encrypted_secret,
+            expiration=datetime.now(timezone.utc) + timedelta(seconds=TTL_EXPIRE_SECONDS),
+        )
+        await self.__repository.create_secret(secret_instance)
         return secret_key
 
     async def get_secret(self, secret_key: str, passphrase: str) -> Optional[str]:
-        secret = await self.repository.get(secret_key)
+        secret = await self.__repository.get_secret(secret_key)
         if secret is None:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Secret not found")
 
@@ -36,5 +41,5 @@ class SecretService:
         except InvalidToken:
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid input data")
 
-        await self.repository.delete(secret_key)
+        await self.__repository.delete_secret(secret_key)
         return decrypted_secret
