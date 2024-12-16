1f508b31f43231426239ae8f40821abec7ebbe27
pecheritsa <pecheritsa.roman@gmail.com>
2024-12-17 01:10:37 +0300

    changed ttl indexes expireAfterSeconds

--- app/repository/secret_repository.py
+++ app/repository/secret_repository.py
@@ -2,6 +2,7 @@ from typing import Optional
 
 from motor.motor_asyncio import AsyncIOMotorCollection
 
+from app.config import TTL_EXPIRE_SECONDS
 from app.models.secret_model import Secret
 
 
@@ -12,7 +13,7 @@ class SecretRepository:
     async def initialize_indexes(self):
         existing_indexes = await self.__collection.index_information()
         if "expiration_1" not in existing_indexes:
-            await self.__collection.create_index([("expiration", 1)], expireAfterSeconds=1)
+            await self.__collection.create_index([("expiration", 1)], expireAfterSeconds=int(TTL_EXPIRE_SECONDS))
 
     async def create_secret(self, secret: Secret) -> None:
         secret_dict = secret.model_dump()
