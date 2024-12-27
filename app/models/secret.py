ef2709baa41990d8511d9da38eb395461b5b1a23
pecheritsa <pecheritsa.roman@gmail.com>
2024-12-27 23:35:34 +0300

    add docs

--- app/models/secret.py
+++ app/models/secret.py
@@ -4,23 +4,43 @@ from pydantic import BaseModel
 
 
 class Secret(BaseModel):
-    secret_key: str
-    secret: str
-    expiration: datetime
+    """
+    Модель для представления секрета с ключом, секретным значением и временем истечения.
+    """
+
+    secret_key: str  # Ключ секрета
+    secret: str  # Секретное значение
+    expiration: datetime  # Время истечения секрета
 
 
 class SecretRequest(BaseModel):
-    secret: str
-    passphrase: str
+    """
+    Модель для запроса секрета, содержащего секретное значение и кодовую фразу.
+    """
+
+    secret: str  # Секретное значение
+    passphrase: str  # Кодовая фраза для доступа к секрету
 
 
 class PassphraseRequest(BaseModel):
-    passphrase: str
+    """
+    Модель для запроса кодовой фразы.
+    """
+
+    passphrase: str  # Кодовая фраза
 
 
 class SecretKeyResponse(BaseModel):
-    secret_key: str
+    """
+    Модель для ответа, содержащего ключ секрета.
+    """
+
+    secret_key: str  # Ключ секрета
 
 
 class SecretResponse(BaseModel):
-    secret: str
+    """
+    Модель для ответа, содержащего секретное значение.
+    """
+
+    secret: str  # Секретное значение
