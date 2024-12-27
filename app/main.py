ae0e420843a1e21dff3541f8dd94dcab68fd7bc6
pecheritsa <pecheritsa.roman@gmail.com>
2024-12-27 23:50:01 +0300

    add docs, typing

--- app/main.py
+++ app/main.py
@@ -13,6 +13,12 @@ from app.models.user import UserRequest
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    """
+    Асинхронный контекст для инициализации и закрытия сервисов и репозиториев.
+
+    Этот контекст управляет жизненным циклом приложения. Он создает и инициализирует сервисы и репозитории при старте
+    приложения, а затем закрывает их при завершении работы приложения.
+    """
     secret_repository, secret_service = create_secret_service_and_repository(
         mongodb_uri=MONGODB_URI, db_name=DATABASE_NAME, salt=SALT
     )
@@ -37,12 +43,28 @@ app.add_exception_handler(JWTDecodeError, jwt_decode_error_handler)
 
 @app.post("/register", tags=["Authentication"])
 async def register_user(request: UserRequest) -> dict:
+    """
+    Регистрация нового пользователя.
+
+    Этот эндпоинт принимает запрос на регистрацию пользователя, хеширует его пароль и сохраняет данные в базе.
+
+    :param request: Данные о пользователе (имя пользователя и пароль).
+    :return: Ответ с сообщением об успешной регистрации.
+    """
     await app.state.user_service.register_user(request.username, request.password)
     return {"message": "User registered successfully"}
 
 
 @app.post("/login", tags=["Authentication"])
 async def login_user(request: UserRequest) -> dict:
+    """
+    Аутентификация пользователя.
+
+    Этот эндпоинт проверяет учетные данные пользователя (имя пользователя и пароль) и генерирует токен доступа.
+
+    :param request: Данные для аутентификации пользователя (имя пользователя и пароль).
+    :return: Ответ с токеном доступа.
+    """
     token = await app.state.user_service.authenticate_user(request.username, request.password)
     return {"access_token": token}
 
@@ -51,6 +73,15 @@ async def login_user(request: UserRequest) -> dict:
 async def generate_secret(
     request: SecretRequest, dependencies=Depends(security.access_token_required)
 ) -> SecretKeyResponse:
+    """
+    Генерация секрета.
+
+    Этот эндпоинт позволяет пользователю с действительным токеном генерировать секрет и получать уникальный ключ.
+
+    :param request: Запрос на генерацию секрета с кодовой фразой.
+    :param dependencies: Зависимость для проверки токена доступа.
+    :return: Ответ с уникальным ключом для доступа к секрету.
+    """
     secret_key = await app.state.secret_service.generate_secret(request.secret, request.passphrase)
     return SecretKeyResponse(secret_key=secret_key)
 
@@ -59,5 +90,16 @@ async def generate_secret(
 async def get_secret(
     secret_key: str, request: PassphraseRequest, dependencies=Depends(security.access_token_required)
 ) -> SecretResponse:
+    """
+    Получение секрета.
+
+    Этот эндпоинт позволяет пользователю с действительным токеном и правильной кодовой фразой получить секрет,
+    используя уникальный ключ.
+
+    :param secret_key: Ключ для доступа к секрету.
+    :param request: Запрос с кодовой фразой для расшифровки секрета.
+    :param dependencies: Зависимость для проверки токена доступа.
+    :return: Ответ с расшифрованным секретом.
+    """
     secret = await app.state.secret_service.get_secret(secret_key, request.passphrase)
     return SecretResponse(secret=secret)
