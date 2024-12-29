1cc28a49f895399f46d3fbac608bf411fa58093e
pecheritsa <pecheritsa.roman@gmail.com>
2024-12-29 20:01:02 +0300

    add MessageResponse model

--- app/models/user.py
+++ app/models/user.py
@@ -1,7 +1,8 @@
+import re
 from typing import Optional
 
 from bson import ObjectId
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 
 
 class UserRequest(BaseModel):
@@ -11,6 +12,24 @@ class UserRequest(BaseModel):
 
     username: str
     password: str
+
+    @field_validator("username")
+    def username_min_length(cls, v):
+        if len(v) < 3:
+            raise ValueError("Username must be at least 3 characters long")
+        return v
+
+    @field_validator("password")
+    def password_complexity(cls, v):
+        if len(v) < 6:
+            raise ValueError("Password must be at least 6 characters long")
+        if not re.search(r"\d", v):
+            raise ValueError("Password must contain at least one digit")
+        if not re.search(r"[A-Z]", v):
+            raise ValueError("Password must contain at least one uppercase letter")
+        if not re.search(r"[\W_]", v):
+            raise ValueError("Password must contain at least one special character")
+        return v
 
 
 class User(BaseModel):
@@ -28,3 +47,9 @@ class User(BaseModel):
         """
 
         json_encoders = {ObjectId: str}
+
+
+class MessageResponse(BaseModel):
+    """Модель ответа"""
+
+    message: str
