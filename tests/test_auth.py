eb84e15c960c7a5d724e3ab12d0cb2d33fdc49bd
pecheritsa <pecheritsa.roman@gmail.com>
2025-01-02 15:39:30 +0300

    add test test_register_user_with_invalid_data

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -48,3 +48,21 @@ async def test_login_invalid_credentials(setup_service: None) -> None:
 
     assert response.status_code == 401
     assert response.json() == {"detail": "Invalid credentials"}
+
+
+@pytest.mark.anyio
+async def test_register_user_with_invalid_data(setup_service: None) -> None:
+    """
+    Проверяет регистрацию пользователя с некорректными данными.
+    При попытке зарегистрировать пользователя с именем, которое слишком короткое,
+    или паролем, который не соответствует требованиям, ожидается ошибка с кодом 422
+    и соответствующим сообщением о ошибке.
+    """
+    username = "us"  # Имя пользователя слишком короткое (меньше 3 символов)
+    password = "12345"  # Пароль не содержит заглавных букв и специальных символов
+
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
+        response = await ac.post("/register", json={"username": username, "password": password})
+
+    assert response.status_code == 422
+
